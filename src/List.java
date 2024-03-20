5070ae3492d4820333d2a6dcaf1c2fabe2c89f52
Aleksei Kiselev <Aleksei_Kiselev@epam.com>
2024-03-20 19:06:51 +0300

    Initial commit

--- src/List.java
+++ src/List.java
@@ -1,4 +1,3 @@
-
 public class List {
     private Node head, tail;
     private int size;
@@ -35,27 +34,44 @@ public class List {
                     tail = null;
                 }
             }
-            else{
+            else {
                 Node ptr = head;
-                tmp = 0;
+                int tmp = 0;
 
-                while(ptr != null){
-                    if (tmp == index - 1) {
-                        if (ptr.next == tail) {
-                            tail = ptr;
-                        }
-                        ptr.next = ptr.next.next;
-                        break;
-                    }
+                for (int i = 0; i < index - 1; i++) {
                     tmp++;
                     ptr = ptr.next;
                 }
+
+                if (ptr.next == tail) {
+                    tail = ptr;
+                }
+                ptr.next = ptr.next.next;
+
             }
             size--;
             return true;
         }
     }
-
+    public boolean edit_el(int num, int index)
+    {
+        if(index >= size)
+            return false;
+        else {
+            if (index == 0) {
+                head = head.next;
+            }
+            else {
+                Node ptr = head;
+                int tmp = 0;
+                for (int i = 0; i < index; i++) {
+                    ptr = ptr.next;
+                }
+                ptr.info = num;
+            }
+            return true;
+        }
+    }
     public int get_num(int index){
         Node ptr = head;
         int tmp = 0;
@@ -72,5 +88,14 @@ public class List {
 
     public int getSize() {
         return size;
+    }
+    public String to_str() {
+        Node ptr = head;
+        String res = "";
+        for(int i = 0; i < size; i++){
+            res += ptr.info + " ";
+            ptr = ptr.next;
+        }
+        return res;
     }
 }
